f15dc72cc8d9ec9efce2c8ba48bba5f10691551d
unknown <hochaucan@gmail.com>
2017-08-11 22:53:09 +0700

    TestMap1

--- screens/Testing.js
+++ screens/Testing.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import {
-View
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Animated,
 }
-from 'react-native';
+  from 'react-native';
+import MapView from 'react-native-maps';
+import flagPinkImg from '../assets/images/flag-pink.png';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
+
 
 export default class Testing extends React.Component {
   static navigationOptions = {
@@ -11,13 +28,104 @@ export default class Testing extends React.Component {
     header: null,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markers: [],
+    };
+
+    this.onMapPress = this.onMapPress.bind(this);
+
+    const panX = new Animated.Value(0);
+    const panY = new Animated.Value(0);
+  }
+
+  onMapPress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: `foo${id++}`,
+        },
+      ],
+    });
+  }
+
+
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
     return (
-      <View>
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={this.state.region}
+          onPress={this.onMapPress}
+        >
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              title={marker.key}
+              image={flagPinkImg}
+              key={marker.key}
+              coordinate={marker.coordinate}
+            />
+          ))}
+        </MapView>
 
+        <View>
+          <Text style={styles.fontColor}>Nguyen Thi Hoang Oanh</Text>
+        </View>
       </View>
     );
   }
 }
+
+// CustomMarkers.propTypes = {
+//   provider: MapView.ProviderPropType,
+// };
+
+const styles = StyleSheet.create({
+  fontColor: {
+    marginTop: 50,
+    color: 'red',
+    backgroundColor: 'transparent',
+  },
+
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+});
